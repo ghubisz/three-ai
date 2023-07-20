995d501c739d856d7993039a3fc84b0ae5afc095
ghubisz <gab.op.pl@op.pl>
2023-07-20 23:03:08 +0200

    ..

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import {
+    headContainerAnimation,
+    headContentAnimation,
+    headTextAnimation,
+    slideAnimation
+ } from '../config/motion';
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
