10318f4a26c43926ab94d0c6597e93de77fa8e9c
ghubisz <gab.op.pl@op.pl>
2023-07-21 19:56:48 +0200

    ..

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { motion, AnimatePresence } from 'framer-motion';
 import { useSnapshot } from 'valtio';
 
+import state from '../store';
+
 import {
     headContainerAnimation,
     headContentAnimation,
@@ -10,9 +12,23 @@ import {
  } from '../config/motion';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    const snap = useSnapshot(state);
+
+    return (
+        <AnimatePresence>
+            {snap.intro && (
+                <motion.section className="home" {...slideAnimation('left')}>
+                    <motion.header>
+                        <img 
+                            src='./threejs.png'
+                            alt="logo"
+                            className="w-8 h-8 object-contain"
+                        />
+                    </motion.header>
+                </motion.section>
+            )}
+        </AnimatePresence>
+    )
 }
 
 export default Home
