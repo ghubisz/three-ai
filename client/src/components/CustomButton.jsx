b09d9e6675fc47d6c953104f745c97fb75b3d23d
ghubisz <gab.op.pl@op.pl>
2023-07-24 16:16:22 +0200

    ..

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -17,6 +17,7 @@ const CustomButton = ({ type, title, customStyles, handleClick}) =>
     <button 
         className={'px-2 py-1.5 flex-1 rounded-md ${customStyles}'}
         style={generateStyle(type)}
+        onClick={handleClick}
     >
         {title}
     </button>
