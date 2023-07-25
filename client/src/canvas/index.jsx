1eaf3e2f67a591ae8d8067016739ae3c67a1d320
ghubisz <gab.op.pl@op.pl>
2023-07-25 12:42:17 +0200

    ..to debug constants.js

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,9 +1,20 @@
 import { Canvas } from '@react-three/fiber';
 import { Environment, Center } from '@react-three/drei';
 
+import Shirt from './Shirt';
+import Backdrop from './Backdrop';
+import CameraRig from './CameraRig';
+
 const CanvasModel = () => {
   return (
-    <div>index</div>
+    <Canvas>
+      <ambientLight intensity={0.5}/>
+      <Environment preset="city" />
+
+      <CameraRig>
+        <Backdrop />
+      </CameraRig>
+    </Canvas>
   )
 }
 
