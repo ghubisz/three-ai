98727afb94ec90b7ef2927040656bfbdaae9a9a0
ghubisz <gab.op.pl@op.pl>
2023-07-26 13:20:24 +0200

    ..

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useSnapshot } from 'valtio'
+import state from '../store';
+
 
 const Tab = () => {
+  const snap = useSnapshot(state);
   return (
     <div>Tab</div>
   )
