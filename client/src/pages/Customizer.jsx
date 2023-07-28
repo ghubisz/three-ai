076047e8edf0c7c1d99654e84fcbf34e555b4540
ghubisz <gab.op.pl@op.pl>
2023-07-28 16:44:19 +0200

    ..

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -54,9 +54,12 @@ const Customizer = () => {
                   <Tab
                     key={tab.name}
                     tab={tab}
-                    handleClick = {() => {}}
+                    handleClick = {() => setActiveEditorTab(tab.name)}
                   />
                 ))}
+
+                {generateTabContent()}
+
               </div>
             </div>
 
