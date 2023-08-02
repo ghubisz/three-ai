9c7eaf798a243b099abb0a422331d2f0e37fc499
ghubisz <gab.op.pl@op.pl>
2023-08-02 21:04:09 +0200

    ..

--- server/index.js
+++ server/index.js
@@ -4,3 +4,14 @@ import cors from 'cors';
 
 import dalleRoutes from './routes/dalle.routes.js';
 
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limig: "50mb" }))
+
+app.get('/', (req, res) => {
+    res,status(200).json({ message: "Hello from DALL.E" })
+})
+
+app.listen(8080, () => console.log('Server has started on port 8080'))
